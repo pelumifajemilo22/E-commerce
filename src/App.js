9a23af827e90d936fa995deb00d861ac1f53feda
pelumi <you@example.com>
2020-05-11 06:18:48 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import  React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom';
+import logo from "./logo.svg";
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from './components/NavBar';
+import Productlist from './components/Productlist';
+import Cart from './components/Cart/Cart';
+import Details from './components/Details';
+import Default from './components/Default';
+import Modal from './components/Modal'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+ 
+  render() { 
+    return (<>
+    <NavBar/>
+    <Switch>
+    <Route exact path="/" component={Productlist}/>
+    <Route exact path="/details" component={Details}/>
+    <Route exact path="/cart" component={Cart}/>
+    <Route component={Default}/>
+    </Switch>
+    <Modal/>
+    </> );
+  }
 }
-
+ 
 export default App;
